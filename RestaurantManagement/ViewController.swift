41e6e75c1d40ad517d05445f93dfef851b8650d9
muziruo <1149354821@qq.com>
2017-10-24 15:45:15 +0800

    Initial Commit

---
+++ RestaurantManagement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RestaurantManagement
+//
+//  Created by 李祎喆 on 2017/10/24.
+//  Copyright © 2017年 李祎喆. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
